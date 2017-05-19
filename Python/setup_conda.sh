adc672ab508e059c811f68d9a6d9b69f34e9ff02
Matthias Feurer <feurerm@informatik.uni-freiburg.de>
2017-05-19 13:16:18 +0200

    update conda installation script

--- Python/setup_conda.sh
+++ Python/setup_conda.sh
@@ -1,5 +1,5 @@
 MINICONDA_URL=https://repo.continuum.io/miniconda/Miniconda3-4.3.14-Linux-x86.sh
-INSTALLATION_DIR=~/miniconda3
+INSTALLATION_DIR=~/Miniconda3-4.3.14-Linux-x86
 ENV_NAME=openml_study14
 
 if [ ! -d "$INSTALLATION_DIR" ]; then
@@ -12,7 +12,7 @@ export PATH="$INSTALLATION_DIR/bin:$PATH"
 conda env create -f environment.yml
 source activate openml_study14
 
-pip install xmltodict==0.11.0 requests==2.14.2
+pip install xmltodict==0.11.0
 pip install git+https://github.com/renatopp/liac-arff@6090cbadb489250db42a499d0094828c1e664ef2
 
 # TODO install latest OpenML version
