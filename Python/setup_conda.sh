87144157762369eb2a5861ca02bc03a1cce0af27
Matthias Feurer <feurerm@informatik.uni-freiburg.de>
2017-06-12 18:40:45 +0200

    update liac-arff dependency

--- Python/setup_conda.sh
+++ Python/setup_conda.sh
@@ -12,8 +12,7 @@ export PATH="$INSTALLATION_DIR/bin:$PATH"
 conda env create -f environment.yml
 source activate openml_study14
 
-pip install xmltodict==0.11.0
-pip install git+https://github.com/renatopp/liac-arff@6090cbadb489250db42a499d0094828c1e664ef2
+pip install xmltodict==0.11.0 liac-arff==2.1.1
 
 # TODO install latest OpenML version
 
